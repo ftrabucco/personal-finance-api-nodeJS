39d0b680ced8d62276f60e4c82851e9b0a60b18e
Fran <trabucco.francisco@gmail.com>
2026-04-25 13:06:15 -0300

    fix: always log to console in containers

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -12,18 +12,13 @@ const logger = createLogger({
     format.json()
   ),
   transports: [
-    new transports.File({ filename: 'logs/error.log', level: 'error' }),
-    new transports.File({ filename: 'logs/combined.log' })
+    new transports.Console({
+      format: format.combine(
+        format.colorize(),
+        format.simple()
+      )
+    })
   ]
 });
-
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new transports.Console({
-    format: format.combine(
-      format.colorize(),
-      format.simple()
-    )
-  }));
-}
 
 export default logger;
